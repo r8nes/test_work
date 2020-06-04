d9ba19c38946d1975ecb0c9ee72df0e0da154f57
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-06-04 22:09:20 +0300

    Redesign of UI.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const container = document.querySelector(".container");
+const list = document.querySelector(".wrapper");
 
 async function getData(url) {
   const response = await fetch(url);
@@ -12,40 +12,40 @@ async function getData(url) {
 
   return await response.json();
 }
-function createMainRow() {
-  const mainRow = `
-<div class="main-row">
-         <div class="column column-1"></div>
-         <div class="column column-2"></div>
-         <div class="column column-3"></div>
-      </div>`;
+// function createMainRow() {
+//   const mainRow = `
+// <div class="main-row">
+//          <div class="column column-1"></div>
+//          <div class="column column-2"></div>
+//          <div class="column column-3"></div>
+//       </div>`;
 
-  container.insertAdjacentHTML("beforeend", mainRow);
-}
+//   container.insertAdjacentHTML("beforeend", mainRow);
+// }
 function createRow({ ID, Name, Age }) {
   const row = `
-<div class="row">
-    <div class="column column-1">${ID}</div>
-    <div class="column column-2">${Name}</div>
-    <div class="column column-3">${Age}</div>
-</div>
+  <li>
+    <p>${ID}</p>
+    <p>${Name}</p>
+    <p>${Age}</p>
+  </li>
 `;
-container.insertAdjacentHTML('beforeend', row);
+  list.insertAdjacentHTML("beforeend", row);
 }
 
 function initial() {
-   getData("./db/test.json").then((data) => {
-      data.forEach(createRow);
-   });
+  getData("./db/test.json").then((data) => {
+    data.forEach(createRow);
+  });
 }
 
 initial();
 
-let tast = [{'ID':1, 'Some':'Reach'},{'2':2}]
+let tast = [{ ID: 1, Some: "Reach" }, { "2": 2 }];
 
-function someTesting ( arr ) {
-   let may = new Map(Object.keys(arr[0]));
-   console.log(may)
+function someTesting(arr) {
+  let may = new Map(Object.keys(arr[0]));
+  console.log(may);
 }
 
 someTesting(tast);
